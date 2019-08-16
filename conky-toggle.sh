dc40401af3a573f1a348b0094246f19a6c2647a9
Ken Gilmer <kgilmer@gmail.com>
2019-08-16 08:59:31 -0700

    Changes to support https://github.com/regolith-linux/regolith-desktop/issues/91.

--- conky-toggle.sh
+++ conky-toggle.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-# Check if gedit is running
-# -x flag only match processes whose name (or command line if -f is
-# specified) exactly match the pattern. 
+# If conky is running, kill it.  Otherwise start it.
 
 if pgrep -x "conky" > /dev/null
 then
--- regolith-ftue.sh
+++ regolith-ftue.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # This script will be called after i3 starts. It's purpose is to do things
-# that only make sense for the first time the user starts Regolith
-# for a particular version.  If this involves more than conky
+# that only make sense for the first time the user starts Regolith.
+# If this involves more than conky
 # in the future it should be pulled out into a separate package.
 
-PKG_VERSION=`dpkg -s regolith-i3-wm | grep '^Version:' | awk '{print $2}'`
-UPDATE_FLAG_PATH="$HOME/.config/i3-regolith/ftue-flag-$PKG_VERSION"
+UPDATE_FLAG_DIR="$HOME/.config/regolith/flags"
+UPDATE_FLAG_PATH="$UPDATE_FLAG_DIR/show-shortcuts"
 
 if [ ! -f $UPDATE_FLAG_PATH ]; then	
 	/usr/bin/conky-toggle.sh
+
+	mkdir -p $UPDATE_FLAG_DIR
 	touch $UPDATE_FLAG_PATH
 fi
